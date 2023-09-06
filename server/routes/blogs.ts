fe6ba0f341aa1fbbec2942b87963417a2e42859f
Luth Andyka <luthandyka.business@gmail.com>
2023-09-06 18:32:42 +0800

    add cache for blogs

--- server/routes/blogs.ts
+++ server/routes/blogs.ts
@@ -3,13 +3,16 @@ import fs from "fs";
 import path from 'path';
 import { marked } from 'marked';
 import _ from "lodash";
+import SimpleCache from '../lib/SimpleCache';
 
-const ARTICLE_DIR = path.join(__dirname, "../articles");
+const BLOGS_DIR = path.join(__dirname, "../blogs");
+const BLOGS_TTL_MS = 5 * 60 * 1000 // 5 minutes
 
 const router = express.Router();
+const cache = new SimpleCache<string>(BLOGS_TTL_MS);
 
 router.get('/', (req: Request, res: Response) => {
-    fs.readdir(ARTICLE_DIR, (err, files) => {
+    fs.readdir(BLOGS_DIR, (err, files) => {
         if (!_.isNull(err)) {
             return res.redirect('/500');
         }
@@ -25,16 +28,24 @@ router.get('/', (req: Request, res: Response) => {
 router.get('/:slug', (req: Request, res: Response) => {
     const { slug } = req.params;
 
+    // check cache for parsed blog
+    const cachedHtml = cache.get(slug);
+    if (!_.isUndefined(cachedHtml)) {
+        return res.render('blog', { html: cachedHtml})
+    }
+
     // find file that matches the slug
-    const articlePath = path.join(ARTICLE_DIR, `${slug}.md`);
+    const blogPath = path.join(BLOGS_DIR, `${slug}.md`);
 
     // TODO use async/await syntax instead
-    fs.readFile(articlePath, (err, data) => {
+    fs.readFile(blogPath, (err, data) => {
         if (!_.isNull(err)) {
             return res.redirect('/404');
         }
 
         const html = marked.parse(data.toString())
+        cache.set(slug, html);
+
         return res.render('blog', { html })
     })
 });
