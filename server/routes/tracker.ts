62c3b2265b4f9da0e876516327c27e2c79bb37ad
driedwater <88548136+driedwater@users.noreply.github.com>
2023-09-10 17:23:56 +0800

    UPDATE: Daily reward checks for date claimed

--- server/routes/tracker.ts
+++ server/routes/tracker.ts
@@ -32,7 +32,14 @@ router.post('/setlimit', checkAuthenticated, async (req: Request, res: Response)
     const filter = { username : req.user!.username }
     const update = { gaming_duration_limit_ms : req.body.gamingHourLimit }
     const currentUser = await  User.findOneAndUpdate(filter, update)
-    currentUser.save()
+
+    if (currentUser === null) {
+        console.log(currentUser)
+        return;
+    } else {
+        currentUser.save()
+    }
+    
     return res.render('setLimit');
 });
 
@@ -43,19 +50,42 @@ router.get('/dailyreward', checkAuthenticated, async (req: Request, res: Respons
 router.post('/dailyreward', checkAuthenticated, async (req: Request, res: Response) => {
     const filter = { username : req.user!.username }
     const currentUser = await  User.findOne(filter)
+    const currentDateTime = new Date()
+    const currentDate = new Date(currentDateTime.toDateString())
 
-    if (currentUser.claimed_coins_today == 'yes') {
-        return res.redirect('nocoins')
+    if (currentUser === null) {
+        console.log(currentUser)
+        return;
     } else {
-        currentUser.coins = currentUser.coins + 10
-        currentUser.claimed_coins_today = 'yes'
-        currentUser.save()
-        return res.redirect('dailyreward')
+        if (formatDate(currentUser.claimed_coins_date) === formatDate(currentDate)) {
+            return res.redirect('nocoins')
+            
+        } else {
+            currentUser.coins = currentUser.coins + 10
+            currentUser.claimed_coins_date = currentDate
+            currentUser.save()
+            return res.redirect('dailyreward')
+        }
     }
+    
 })
 
 router.get('/nocoins', checkAuthenticated, async (req: Request, res: Response) => {
     res.render('noCoins')
 })
+
+function formatDate(date: Date) {
+    const newDate = new Date(date)
+    var month = '' + (newDate.getMonth() + 1)
+    var day = '' + newDate.getDate()
+    var year = newDate.getFullYear()
+
+      if (month.length < 2) 
+        month = '0' + month;
+      if (day.length < 2) 
+        day = '0' + day;
+    
+      return [year, month, day].join('-');
+    }
 
 export default router;
